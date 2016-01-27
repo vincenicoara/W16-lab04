ba63966726f317236acbdb29e85c3d1ad995ed80
Chang Rey Tang <changreytang@umail.ucsb.edu>
2016-01-26 22:07:08 -0800

    Finished iphone drawing implementation

--- src/edu/ucsb/cs56/w16/drawings/changreytang/advanced/IPhone.java
+++ src/edu/ucsb/cs56/w16/drawings/changreytang/advanced/IPhone.java
@@ -9,15 +9,15 @@ import edu.ucsb.cs56.w16.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.w16.drawings.utilities.GeneralPathWrapper;
 
 /**
-   A vector drawing of an touch screen phone using
-   the Shape interface, and so can be drawn, as well as
-   rotated, scaled, etc.
+   A vector drawing of an Apple iPhone which extends 
+   TouchScreenPhone using the Shape interface, and 
+   so can be drawn, as well as rotated, scaled, etc.
       
    @author Chang Rey Tang 
    @version for CS56, W16, UCSB
    
 */
-public class TouchScreenPhone extends GeneralPathWrapper implements Shape
+public class IPhone extends TouchScreenPhone implements Shape
 {
     /**
        Constructor
@@ -26,25 +26,55 @@ public class TouchScreenPhone extends GeneralPathWrapper implements Shape
        @param y y coord of upper left corner of phone from the top 
        @param height height of the phone 
     */
-    public TouchScreenPhone(double x, double y, double height)
+    public IPhone(double x, double y, double height)
     {
+	super(x,y,height);
 	
-	double phoneHeight = height;
-	double screenHeight = 0.9*phoneHeight;
-	double phoneWidth = 0.6*height;
-	double screenWidth = 0.9*phoneWidth;
-        
-        
-        
-        Rectangle2D.Double phoneBody = 
-            new Rectangle2D.Double(x, y, phoneWidth, phoneHeight); 
-	
-	Rectangle2D.Double phoneScreen =
-	    new Rectangle2D.Double(x+(phoneHeight*0.025), y+(phoneWidth*0.025), 
-				   screenWidth, screenHeight);        
+	GeneralPath gp = this.get();	
 
-        GeneralPath phone = this.get();
-        phone.append(phoneBody, false);
-        phone.append(phoneScreen, false);
+	double phoneHeight = height;
+	double phoneWidth = 0.6*height;
+	double xhalf = phoneWidth/2;
+	double yhalf = phoneHeight/2;
+	
+	Line2D.Double line1 = new Line2D.Double(x+xhalf,y+yhalf+yhalf*0.2,
+			       		        x+xhalf+xhalf*0.3,y+yhalf+yhalf*0.25);
+	Line2D.Double line2 = new Line2D.Double(x+xhalf+xhalf*0.3,y+yhalf+yhalf*0.25,
+					        x+xhalf+xhalf*0.45,y+yhalf);
+	Line2D.Double line3 = new Line2D.Double(x+xhalf+xhalf*0.45,y+yhalf,
+						x+xhalf+xhalf*0.25,y+yhalf-yhalf*0.2);
+	Line2D.Double line4 = new Line2D.Double(x+xhalf+xhalf*0.25,y+yhalf-yhalf*0.2,
+						x+xhalf,y+yhalf-yhalf*0.15);
+	Line2D.Double line5 = new Line2D.Double(x+xhalf,y+yhalf-yhalf*0.15,
+						x+xhalf-xhalf*0.25,y+yhalf-yhalf*0.2);
+	Line2D.Double line6 = new Line2D.Double(x+xhalf-xhalf*0.25,y+yhalf-yhalf*0.2,
+						x+xhalf-xhalf*0.45,y+yhalf);
+	Line2D.Double line7 = new Line2D.Double(x+xhalf-xhalf*0.45,y+yhalf,
+						x+xhalf-xhalf*0.3,y+yhalf+yhalf*0.25);
+	Line2D.Double line8 = new Line2D.Double(x+xhalf-xhalf*0.3,y+yhalf+yhalf*0.25,
+						x+xhalf,y+yhalf+yhalf*0.2);
+	Line2D.Double line9 = new Line2D.Double(x+xhalf,y+yhalf-yhalf*0.2,
+						x+xhalf,y+yhalf-yhalf*0.25);
+	Line2D.Double line10 = new Line2D.Double(x+xhalf,y+yhalf-yhalf*0.25,
+						 x+xhalf+xhalf*0.1,y+yhalf-yhalf*0.3);
+	Line2D.Double line11 = new Line2D.Double(x+xhalf+xhalf*0.1,y+yhalf-yhalf*0.3,
+						 x+xhalf+xhalf*0.1,y+yhalf-yhalf*0.25);
+	Line2D.Double line12 = new Line2D.Double(x+xhalf+xhalf*0.1,y+yhalf-yhalf*0.25,
+						 x+xhalf,y+yhalf-yhalf*0.2);
+	
+
+        GeneralPath apple = this.get();
+        apple.append(line1, false);
+	apple.append(line2, false);
+	apple.append(line3, false);
+	apple.append(line4, false);
+	apple.append(line5, false);
+	apple.append(line6, false);
+	apple.append(line7, false);
+	apple.append(line8, false);
+	apple.append(line9, false);
+	apple.append(line10, false);
+	apple.append(line11, false);
+	apple.append(line12, false);
     }
 }
