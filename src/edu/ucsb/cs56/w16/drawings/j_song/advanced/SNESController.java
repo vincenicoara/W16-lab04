312bdee369aeb386b6b72203094ceb3e3956fff0
Joseph Song <j_song@umail.ucsb.edu>
2016-01-29 03:46:17 -0800

    Improved javadoc comments

--- src/edu/ucsb/cs56/w16/drawings/j_song/advanced/SNESController.java
+++ src/edu/ucsb/cs56/w16/drawings/j_song/advanced/SNESController.java
@@ -9,10 +9,8 @@ import edu.ucsb.cs56.w16.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.w16.drawings.utilities.GeneralPathWrapper;
 
 /**
-   A vector drawing of a controller that implements
-   the Shape interface, and so can be drawn, as well as
-   rotated, scaled, etc.
-      
+   Drawing of an enhanced controller with two additional buttons.     
+ 
    @author Joseph Song
    @version for CS56, W16, UCSB
    
